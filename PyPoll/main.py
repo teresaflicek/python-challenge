9381157a61f06733218e292fa1f555715bf04f57
teresaflicek <69260261+teresaflicek@users.noreply.github.com>
2021-01-12 19:11:20 -0600

    error with votes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,7 +9,7 @@ election_csv = os.path.join('Resources','election_data.csv')
 total_votes = 0
 cand_names = []
 vote_percents = []
-num_votes = []
+cand_votes = []
 
 
 # read in the CSV file
@@ -31,45 +31,60 @@ with open(election_csv, 'r') as csvfile:
         total_votes += 1
 
         if (total_votes == 1):
-            cand_names.append(row[0])
-            num_votes.append(1)
+            cand_names.append(row[2])
+            cand_votes.append(1)
 
-    # percent votes
-    for votes in num_votes:
+    # percent of votes comparable to total for each candidate
+    for votes in cand_votes:
         percent = (votes / total_votes)*100
         percent = round((percent))
         percent = "%.3f%%" % percent
         vote_percents.append(percent)
 
-print(vote_percents)
+# winner of the election
 
-    # total votes each candidate won
+# variable containing the most votes
+winner_votes = max(cand_votes)
 
-    # winner of the election
+# variables containing the winner name 
+winner_index = cand_votes.index(winner_votes)
+winner_cand = cand_names[winner_index]
 
+#printing the results
+print("Election Results")
+print("-------------------------")
+print(f"Total Votes: {total_votes}")
+for i in range(len(cand_names)):
+    print(f"{cand_names[i]}: {vote_percents[i]} {cand_votes[i]}")
+print(f"Winners: {winner_cand}")
 
 
 
 
 
 # print report with triple f string
-report = (f"""Election Results
--------------------------
-Total Votes: 3521001 {total_votes}
--------------------------
-Khan: 63.000% (2218231)
-Correy: 20.000% (704200)
-Li: 14.000% (492940)
-O'Tooley: 3.000% (105630)
--------------------------
-Winner: Khan
--------------------------
-""")
+# report = (f"""
 
-print(report)
+# Election Results
+# -------------------------
+# Total Votes: 3521001 {total_votes}
+# -------------------------
+# Khan: 63.000% (2218231)
+# Correy: 20.000% (704200)
+# Li: 14.000% (492940)
+# O'Tooley: 3.000% (105630)
+# -------------------------
+# Winner: Khan
+# -------------------------
+# """)
 
 # write the results into a text file
-# outputFile = os.path.join('analysis', 'pypoll_report.txt')
+outputFile = os.path.join('analysis', 'pypoll_report.txt')
 
-# with open(outputFile, 'w') as text_file:
-#     text_file.write
+with open(outputFile, 'w') as text_file:
+    text_file.write("Election Results\n")
+    text_file.write("-------------------------\n")
+    text_file.write(f"Total Votes: {total_votes}\n")
+    for i in range(len(cand_names)):
+        text_file.write(f"{cand_names[i]}: {vote_percents[i]} {cand_votes[i]}\n")
+    text_file.write(f"Winners: {winner_cand}\n")
